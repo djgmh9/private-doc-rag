69ad7dd1582639f6a346431ea4d43c00b7318ebc
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2026-02-12 09:03:57 +0800

    Ingest sample OSHA pdf

---
+++ ingest_data.py
@@ -0,0 +1,53 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from uuid import uuid4
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# configuration
+DATA_PATH = r"data"
+CHROMA_PATH = r"chroma_langchain_db"
+
+# initiate the embeddings model
+embeddings_model = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+
+# select a vector store
+
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings_model,
+    persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+)
+
+# initiate the vector store
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings_model,
+    persist_directory=CHROMA_PATH,
+)
+
+# loading the PDF document
+loader = PyPDFDirectoryLoader(DATA_PATH)
+
+raw_documents = loader.load()
+
+# splitting the document
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=100,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+# creating the chunks
+chunks = text_splitter.split_documents(raw_documents)
+
+# creating unique ID's
+uuids = [str(uuid4()) for _ in range(len(chunks))]
+
+# adding chunks to vector store
+vector_store.add_documents(documents=chunks, ids=uuids)
